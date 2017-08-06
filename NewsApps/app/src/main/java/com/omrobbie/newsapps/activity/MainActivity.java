1ade7805616fedf6de8680489d093f7f311cbe8d
omrobbie <putu.roby@yahoo.com>
2017-08-06 17:42:58 +0800

    Kelas IAK Intermediate Pertama - NewsApps

---
+++ NewsApps/app/src/main/java/com/omrobbie/newsapps/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.omrobbie.newsapps.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.omrobbie.newsapps.R;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
